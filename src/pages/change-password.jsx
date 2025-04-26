ff55cbdb924bee83ec949ea3fdd1843b66ddc7b7
darshLogicgo <darsh.logicgo@gmail.com>
2025-04-26 12:20:46 +0530

    your commit message

--- src/pages/change-password.jsx
+++ src/pages/change-password.jsx
@@ -8,7 +8,7 @@ const ChangePassword = () => {
     const [form] = Form.useForm();
 
     const onFinish = (values) => {
-        console.log('Form Values:', values);
+        console.log('Form Valuesss:', values);
     };
 
     return (
